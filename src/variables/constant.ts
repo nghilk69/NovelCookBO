2ee780e5ca8840d3230f22d9e6f094164dc5aa31
nghi12 <nghilk69@gmail.com>
2022-10-25 14:51:33 +0700

    change api url

--- src/variables/constant.ts
+++ src/variables/constant.ts
@@ -7,4 +7,4 @@
 // export const API_URL = process.env.REACT_APP_API_URL || 'https://api.v3.staging.transalliancehub.ql6625.fr/api';
 
 //export const API_URL = process.env.REACT_APP_API_URL || 'https://api.transalliancehub.staging.bfast-vn.net/api';
-export const API_URL = process.env.REACT_APP_API_URL || 'https://api.anime.wildwolves.shop';
+export const API_URL = 'https://api.anime.wildwolves.shop';
